66491551d5bdfbce3fb3d4f3ede3135160ed1298
Ricardo Andrade <ricardo.andrade05@gmail.com>
2022-12-02 21:27:26 -0300

    Criacao SellerDaoJdbc e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,15 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+
 public class Program {
 
 	public static void main(String[] args) {
 
+	 SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		
 	}
 
 }
