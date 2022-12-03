080fe24ca53099139dffcda2c5f15aac27584dc1
Ricardo Andrade <ricardo.andrade05@gmail.com>
2022-12-03 16:45:16 -0300

    Implementacao findAll

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,6 +24,12 @@ public class Program {
 			System.out.println(obj);	
 		}
 		
+		System.out.println("=== TESTE 3: seller findAll ====");
+		list = sellerDao.findAll();
+		for(Seller obj:list) {
+			System.out.println(obj);	
+		}
+		
 		
 		
 	}
