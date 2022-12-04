7365b0b5e201fcfef2b343a1d119800c2fce466e
Ricardo Andrade <ricardo.andrade05@gmail.com>
2022-12-04 09:44:00 -0300

    Implementacao update

--- src/application/Program.java
+++ src/application/Program.java
@@ -34,8 +34,13 @@ public class Program {
 		System.out.println("=== TESTE 4: seller insert ====");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
 		sellerDao.insert(newSeller);
-		
 		System.out.println("Inserted! New id=  "+ newSeller.getId());
+		
+		System.out.println("=== TESTE 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 		
 		
 	}
